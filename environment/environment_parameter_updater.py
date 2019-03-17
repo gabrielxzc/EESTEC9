735d4c8f247c311fb0be498ba8936158cbc77e2a
gabrielxzc <codrincojocaru@yahoo.com>
2019-03-17 02:34:59 +0200

    environment optimizations, scorpion down frame

--- environment/environment_parameter_updater.py
+++ environment/environment_parameter_updater.py
@@ -3,6 +3,7 @@ import image_analysis.image_analysys as analysys
 
 
 def update_environment_parameters(frame):
-    # cv2.imshow('client', frame)
-    cv2.imshow('processed', analysys.process_image(frame)['frame'])
+    # processed_results = analysys.process_image(frame)
+    # cv2.imshow('processed', processed_results['frame'])
+    cv2.imwrite('frame-scorpion-down.png', frame)
     cv2.waitKey(1)
