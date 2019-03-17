86748464923be349ac0a04a053ae2f7a10ca704b
gabrielxzc <codrincojocaru@yahoo.com>
2019-03-17 02:39:14 +0200

    frames for lower hp, hp going down

--- environment/environment_parameter_updater.py
+++ environment/environment_parameter_updater.py
@@ -3,7 +3,7 @@ import image_analysis.image_analysys as analysys
 
 
 def update_environment_parameters(frame):
-    # processed_results = analysys.process_image(frame)
-    # cv2.imshow('processed', processed_results['frame'])
-    cv2.imwrite('frame-scorpion-down.png', frame)
+    cv2.imshow('processed', frame)
+    cv2.imwrite('frames/frame-lower-hp-players.png', frame)
+    # print(processed_results)
     cv2.waitKey(1)
